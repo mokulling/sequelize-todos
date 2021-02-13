8ec1b15a2a129a55b814270df528e17c49e54703
mokulling <72229978+mokulling@users.noreply.github.com>
2021-02-13 12:11:41 -0600

    added post request

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,21 @@
 
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 
 // router.get()
 
 // router.post()
+router.post('/api/todos', async (req, res) => {
+
+    try {
+    await db.Todo.create(req.body)
+    res.status(200).send()
+    } catch (err) {
+        res.status(500).send(err)
+        res.end()
+    }
+})
 
 // router.put()
 
