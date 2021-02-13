863cff90d1733f64163ecb53de06c8c903b722a9
mokulling <72229978+mokulling@users.noreply.github.com>
2021-02-13 12:00:21 -0600

    updated view

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM loaded!');
 
+  const form = document.getElementById('todo-form')
+  const newTodoInput = document.querySelector('input.new-item')
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const todoText = newTodoInput.value
+  fetch('/api/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({text})
+  }).then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.error(err))
+
+})
   // your code here...
+
+  
 });
